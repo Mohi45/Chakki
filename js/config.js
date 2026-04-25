09ee1a5530e9227df0c82e0ab4252a7c00ae8e88
unknown <mk908990@gmail.com>
2026-04-25 22:13:55 +0530

    Added

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const ADMIN_USERNAME = "admin";
 const ADMIN_PASSWORD = "Safachatt@1234";
 const UPI_ID = "9758620961-2@ybl";
-const PAYMENT_QR_PATH = "img/qr.jpeg";
+const PAYMENT_QR_PATH = "img/Qr1.jpeg";
 const LOW_STOCK_LIMIT = 50;
